e90084ea859494ee395700664cc0efbf4cbbf238
daniellezra1 <47479546+daniellezra1@users.noreply.github.com>
2020-02-15 01:45:41 +0200

    Update memegen-editor-controller.js

--- js/memegen-editor-controller.js
+++ js/memegen-editor-controller.js
@@ -136,7 +136,7 @@ function uploadImg(elForm, ev) {
 
 function doUploadImg(elForm, onSuccess) {
     var formData = new FormData(elForm)
-    fetch('http://ca-upload.com/here/upload.php', {
+    fetch('https://ca-upload.com/here/upload.php', {
         method: 'POST',
         body: formData
     })
